7d12dd37f440814a47d48aa15e10533bc1a54cf2
martinsejas <martinhugosejas@gmail.com>
2022-05-04 19:48:14 -0300

    can capture and release pokemon properly from detail page

--- src/pages/PokemonPage/PokemonPage.jsx
+++ src/pages/PokemonPage/PokemonPage.jsx
@@ -1,17 +1,65 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState, useEffect, useContext} from 'react'
 import { useNavigate, useParams, useSearchParams } from 'react-router-dom';
 import Header from '../../components/Header/Header'
 import { goToLastPage } from '../../services/Routes/coordinators';
 import { PokemonPageContentDiv, PokemonPageMainDiv } from './styled'
+import { getPokemonDetail } from '../../services/requests';
+import { GlobalContext } from '../../contexts/GlobalContext/GlobalContext'
+
 
 function PokemonPage() {
   const navigate = useNavigate(); 
-
-
+  let [pokemon, setPokemon] = useState({})
+  const {states, setters, requests} = useContext(GlobalContext); 
+  let {setPokemonList, setPokedex} = setters; 
+  let [inPokedex, setInPokedex] = useState(false)
   let {name} = useParams(); 
   const [searchParams, setSearchParams] = useSearchParams(); 
-  let pokedexQuery = searchParams.get("pokedex"); 
- let inPokedex = (pokedexQuery === "true" ? true: false)
+
+  useEffect( ()=> {
+  
+    getPokemonDetail(`https://pokeapi.co/api/v2/pokemon/${name}`, setPokemon); 
+    inPokedex = searchParams.get("pokedex") === "true"
+  }, [])
+
+
+
+
+ const grabPokemon = () => {
+  //inside pokedex going to wild pokemons
+  if(inPokedex) {
+  
+    setPokemonList([...states.pokemonList, {name: name, url: `https://pokeapi.co/api/v2/pokemon/${name}` }])
+
+    let updatedPokedex = states.pokedex.filter( (poke) => {
+      return poke.name !== name
+    })
+
+    setPokedex(updatedPokedex)
+   setInPokedex(!inPokedex); 
+  }
+
+  else {
+    //inside wild pokemons going to pokedex
+    // add to pokedex, remove from wild pokemons
+    setPokedex([...states.pokedex, {name: name, url: `https://pokeapi.co/api/v2/pokemon/${name}` }])
+
+    let updatedWildPokemons = states.pokemonList.filter( (poke) => {
+     
+      return poke.name !== name
+    })
+
+    setPokemonList(updatedWildPokemons);
+    setInPokedex(!inPokedex); 
+    
+  }
+}
+
+
+useEffect( ()=> {
+  getPokemonDetail(`https://pokeapi.co/api/v2/pokemon/${name}`, setPokemon); 
+ 
+}, [states.pokemonList, states.pokedex, inPokedex])
 
   return (
     <PokemonPageMainDiv>
@@ -21,6 +69,7 @@ function PokemonPage() {
         leftButtonClick = { ()=> goToLastPage(navigate)}
         rightButtonExists={true}
         rightButton={ inPokedex ? "Liberar Pokemon": "Capturar Pokemon"}
+        rightButtonClick = { () => grabPokemon()}
         />
   
         <PokemonPageContentDiv>
