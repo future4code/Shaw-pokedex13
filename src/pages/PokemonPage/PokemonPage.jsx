01a8eaad807984a33165da47925ae3c5ad3cdb2d
martinsejas <martinhugosejas@gmail.com>
2022-05-05 11:27:34 -0300

    pagina de detalhes layout

--- src/pages/PokemonPage/PokemonPage.jsx
+++ src/pages/PokemonPage/PokemonPage.jsx
@@ -2,45 +2,87 @@ import React, {useState, useEffect, useContext} from 'react'
 import { useNavigate, useParams, useSearchParams } from 'react-router-dom';
 import Header from '../../components/Header/Header'
 import { goToLastPage } from '../../services/Routes/coordinators';
-import { PokemonPageContentDiv, PokemonPageMainDiv } from './styled'
+import { PokemonPageContentDiv, PokemonPageInfoDiv, PokemonPageMainDiv, PokemonPageTypesDiv } from './styled'
 import { getPokemonDetail } from '../../services/requests';
 import { GlobalContext } from '../../contexts/GlobalContext/GlobalContext'
+import { Button } from '@chakra-ui/react';
 
 
 function PokemonPage() {
   const navigate = useNavigate(); 
-  let [pokemon, setPokemon] = useState({})
+  let [pokemon, setPokemon] = useState({}) 
+
+  let [displayInfoCount, setDisplayInfoCount] = useState(1); 
+
+
   const {states, setters, requests} = useContext(GlobalContext); 
   let {setPokemonList, setPokedex} = setters; 
   let [inPokedex, setInPokedex] = useState(false)
-  let {name} = useParams(); 
+
+
+ // como url da pagina de detalhe eh path="/pokemon/:name", o Router sabe que :name eh uma variavel, e podemos usa-lo no nosso codigo
+ // em este caso :name eh o nosso param, a gente bota o nome do pokemon que a gente quer ver o detalhe
+ // que eh chamado no arquivo coordinators.js, como o nome do pokemon
+
+  let {name} = useParams(); // pegando nome do pokemon, do url, para usar no API
+
+
+  //search param tem nome e valor
+  //no nosso caso para saber se vem do pokedex ou da lista de pokemon, para mostrar o botao (capturar/liberar) corretamente
   const [searchParams, setSearchParams] = useSearchParams(); 
 
+
+
+
+//primeira montagem, pegar dados do pokemon da API usando o nome que foi passado na URL
   useEffect( ()=> {
   
+    //pegando detalhes do pokemon da API pelo nome na url
+    
+    //vamos mandar o url para uma funcao que vai fazer o GET no API, e vamos passar  a funcao que vai guardar o valor do API na variavel pokemon DESTA pagina
     getPokemonDetail(`https://pokeapi.co/api/v2/pokemon/${name}`, setPokemon); 
+
+    //para os botoes de capturar/liberar
+
     let myPokedex = searchParams.get("pokedex") === "true"; 
     setInPokedex(myPokedex)
   }, [])
 
+//PAGINA DO CHARIZARD
 
-
-
+//funcao ela joga pokemon da lista para o pokedex OU do pokedex para a lista (CAPTURAR OU LIBERAR)
  const grabPokemon = () => {
-  //inside pokedex going to wild pokemons
-  if(inPokedex) {
+
+  //PROCESSO DE LIBERAR POKEMON DO POKEDEX::::    POKEDEX -> LISTA DE POKEMON
+  if(inPokedex) { // SE BOOLEAN inPokedex for true, pokemon esta dentro do pokedex e tem que ir para a lista de pokemon
   
+    //Spread para copiar os valores que ja estao dentro e virgula para ADICIONAR este pokemon para a lista
+    //ja joga o charizard para a lista de pokemon selvagem
     setPokemonList([...states.pokemonList, {name: name, url: `https://pokeapi.co/api/v2/pokemon/${name}` }])
 
+
+    //TEM QUE TIRAR DO POKEDEX
+    //filtrar de pokedex, que ele retorne nesta lista TODOS os pokemons que NAO tenham o nome de CHARIZARD volte
+
+
+    // charizard, charmander, bulbasaur, ivysaur
+
+    //filtrar o que seleccionei para desaparecer
+    //charizard nao vai estar mais
     let updatedPokedex = states.pokedex.filter( (poke) => {
-      return poke.name !== name
+      return poke.name !== name //(CHARIZARD)
     })
 
-    setPokedex(updatedPokedex)
-    setInPokedex(!inPokedex); 
+    setPokedex(updatedPokedex) //atualizar pokedex
+
+
+    setInPokedex(!inPokedex); //avisar que saiu do pokedex
   }
 
-  else {
+  //MESMA COISA SENTIDO CONTRARIO
+  //PROCESSO DE CAPTURAR POKEMON PARA O POKEDEX::::    LISTA DE POKEMON -> POKEDEX
+  else 
+  {
     //inside wild pokemons going to pokedex
     // add to pokedex, remove from wild pokemons
     setPokedex([...states.pokedex, {name: name, url: `https://pokeapi.co/api/v2/pokemon/${name}` }])
@@ -50,17 +92,54 @@ function PokemonPage() {
       return poke.name !== name
     })
 
-    setPokemonList(updatedWildPokemons);
-    setInPokedex(!inPokedex); 
+    setPokemonList(updatedWildPokemons); //atualiza a lista
+    setInPokedex(!inPokedex); //avisa que esta no pokedex
     
   }
 }
 
-
+//quando eu tirar ou adicionar pokemon do pokedex, atualizar pagina
 useEffect( ()=> {
   getPokemonDetail(`https://pokeapi.co/api/v2/pokemon/${name}`, setPokemon); 
  
 }, [states.pokemonList, states.pokedex, inPokedex])
+
+
+
+let displayInfo = () => {
+
+  if (displayInfoCount === 3)
+  {
+    setDisplayInfoCount(1)
+  }
+
+  else {
+    setDisplayInfoCount(displayInfoCount + 1)
+  }
+  
+}
+
+let buttonText; 
+
+switch (displayInfoCount) {
+  case 1: 
+    buttonText = "STATS"
+    //logica de mostrar foto
+    break; 
+
+  case 2: 
+  buttonText = "Ataques"
+  //logica de mostrar stats
+  break; 
+
+  case 3: buttonText = "Foto"
+  //logica de mostrar ataques
+  break; 
+
+  default: 
+  buttonText = "STATS"
+}
+
 
   return (
     <PokemonPageMainDiv>
@@ -74,7 +153,28 @@ useEffect( ()=> {
         />
   
         <PokemonPageContentDiv>
+
+          <PokemonPageTypesDiv>
+            <p>{pokemon.types && pokemon.types[0].type.name}</p>
+            <p>{pokemon.types && pokemon.types[1].type.name}</p>
+          </PokemonPageTypesDiv>
+
+
+
+          <PokemonPageInfoDiv>
+
+          </PokemonPageInfoDiv>
+
+
+
+
+          <Button colorScheme="amarelo" onClick= {() => displayInfo()}>{buttonText}</Button>
         
+
+
+
+
+
        </PokemonPageContentDiv>
         
         </PokemonPageMainDiv>
--- src/pages/PokemonPage/styled.js
+++ src/pages/PokemonPage/styled.js
@@ -27,6 +27,45 @@ Button {
 export const PokemonPageContentDiv = styled.div`
 display: flex; 
 flex-direction: column; 
+width: 100%; 
+height: 100%; 
 background-color: ${COLORS.background}; 
+
+align-items: center; 
+justify-content: center; 
+
+Button {
+    padding: 1.5% 7%; 
+    max-width: 12%; 
+    font-size: 1.5rem; 
+    border-radius: 15px; 
+    border: 2px solid black; 
+
+}
+
+`
+
+
+export const PokemonPageTypesDiv = styled.div`
+background-color: ${COLORS.secondary}; 
+display: flex; 
+align-items: center; 
+justify-content: space-between;
+font-size: 2rem; 
+padding: 0% 3%; 
+width: 30%; 
+height: 8%; 
+border: 2px solid black; 
+
+`
+
+export const PokemonPageInfoDiv = styled.div`
+background-color: ${COLORS.primary}; 
+
+margin-top: 2%;
+margin-bottom: 1%; 
+width: 30%; 
+height: 60%; 
+border: 2px solid black; 
 
 `
